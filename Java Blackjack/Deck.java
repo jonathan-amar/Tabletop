465aa2ee30c4e1260abafdc5e06e3d8563465fea
Jonathan Amar <amar.jonathan00@gmail.com>
2023-12-16 00:02:50 -0500

    added constructor for deck

--- Java Blackjack/Deck.java
+++ Java Blackjack/Deck.java
@@ -1,3 +1,16 @@
+import java.util.Stack;
 public class Deck {
-    
+    Stack<Card> deck;
+
+    public Deck(){
+        for(Suit s : Suit.values()){
+            for(Rank r : Rank.values()){
+                deck.add(new Card(s, r));
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        
+    }
 }
